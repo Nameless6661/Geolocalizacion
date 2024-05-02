fca94e015ccf0f522f775d8c15771c64938e2288
Nameless6661 <gus_99999.com>
2024-05-02 15:22:12 -0600

    first commit

--- App.js
+++ App.js
@@ -1,61 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
-import { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
+import MapView,{Marker} from 'react-native-maps';
+import { StyleSheet, Text,View, Dimensions, Button } from 'react-native';
 import * as Location from 'expo-location';
 
-export default function App() {
-  const [location, setLocation] = useState();
-  const [address, setAddress] = useState();
+export default function App(){
+  const[mapRegion, setMapRegion] = useState({
+    latitude:37.78825,
+    longitude:-122.4324,
+    latitudeDelta:0.0922,
+    longitudeDelta:0.0421,
+  });
 
-  Location.setGoogleApiKey("AIzaSyD5GUOMMrDY5Ml8JOQ5j7z7p9f8GaGCDBg");
-
-  useEffect(() => {
-    const getPermissions = async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log("Please grant location permissions");
-        return;
-      }
-
-      let currentLocation = await Location.getCurrentPositionAsync({});
-      setLocation(currentLocation);
-      console.log("Location:");
-      console.log(currentLocation);
-    };
-    getPermissions();
-  }, []);
-
-  const geocode = async () => {
-    const geocodedLocation = await Location.geocodeAsync(address);
-    console.log("Geocoded Address:");
-    console.log(geocodedLocation);
-  };
-
-  const reverseGeocode = async () => {
-    const reverseGeocodedAddress = await Location.reverseGeocodeAsync({
-      longitude: location.coords.longitude,
-      latitude: location.coords.latitude
+  const userLocation = async () => {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if(status !== 'granted'){
+      setErrorMsg('Permiso a la ubicación denegado');
+      
+    }
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy:true});
+    setMapRegion({
+      latitude:location.coords.latitude,
+      longitude:location.coords.longitude,
+      latitudeDelta:0.0922,
+      longitudeDelta:0.0421,
     });
+    console.log(location.coords.latitude, location.coords.longitude);
+  }
 
-    console.log("Reverse Geocoded:");
-    console.log(reverseGeocodedAddress);
-  };
-
-  return (
-    <View style={styles.container}>
-      <TextInput placeholder='Address' value={address} onChangeText={setAddress} />
-      <Button title="Geocode Address" onPress={geocode} />
-      <Button title="Reverse Geocode Current Location" onPress={reverseGeocode} />
-      <StatusBar style="auto" />
+  useEffect (() => {
+    userLocation();
+  }, []);
+  return(
+    <View style={styles.container}> 
+      <MapView style={styles.map}
+        region={mapRegion}
+      >
+        <Marker coordinate={mapRegion} title='Marker' />
+      </MapView>
+      <Button title='Obtener Ubicación' onPress={userLocation}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  map:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
